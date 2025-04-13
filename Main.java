d15183c0aa99bbfc78183431f07e6d7fa3711203
Aagaman <aagamankoirala10@gmail.com>
2025-04-13 10:57:29 +0545

    Imported random and scanner from java util package

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
     }
 }
